5e468b2e6b6265e70e8d8271ba80b478d288ff05
Bartosz Kobylinski <bartoszkobylinski@Bartoszs-Mac-mini.local>
2022-12-03 23:06:54 +0100

    Final solutions to the tasks on day 3

--- day3_aoc.py
+++ day3_aoc.py
@@ -15,6 +15,8 @@ def calculate_total_result(rucksacks_list, alphabet):
 
     result = 0
     for list in rucksacks_list:
+        print(f"that is list: {list}")
+        print(f"that is next list: {next(iter(list))}")
         list = list.strip()
         middle_index = int(len(list)/2)
         list_1 = list[:middle_index]
@@ -23,6 +25,23 @@ def calculate_total_result(rucksacks_list, alphabet):
         common_element = next(iter(common_element))
         point = alphabet.index(common_element) + 1
         result += point
+        time.sleep(2)
     return result
 
-print(f"that is result: {calculate_total_result(extract_input_file(file=FILE), alphabet=ALPHABET)}")
+
+rucksacks_list = extract_input_file(file=FILE)
+rucksacks_list = iter(rucksacks_list)
+result_1 = 0
+while True:
+    temp_list = []
+    try:
+        for _ in range(3):
+            rucksack = next(rucksacks_list).strip()
+            temp_list.append(rucksack)
+    except StopIteration:
+        break
+    common_element = set(temp_list[0]).intersection(temp_list[1],temp_list[2])
+    point = ALPHABET.index(common_element.pop()) +1
+    result_1 +=point
+    
+print(result_1)
